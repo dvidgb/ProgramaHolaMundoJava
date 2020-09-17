547545cde5625403718e965ab15a58b988722619
David Bueno Castro <david.bueno.castro@iesjandula.es>
2020-09-17 13:09:10 +0200

    Colores definidos

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,6 +3,8 @@ public class HolaMundo{
 
 	public stataic void main(String [] args){
 
+	String naranja = "naranja";
+	String azul = "azul";
 	system.out.println("Hola Mundo");
 	system.out.println("Hola Mundo");
 
